bef1e668f70ee4763e684db620485147db1093d5
sudonotfound <koxxxm@icloud.com>
2024-11-14 00:34:04 +0900

    Change the initial values of the program for amount calculation

--- cafe.rb
+++ cafe.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+require 'debug'
 
 DRINKS = [
   { name: 'コーヒー', price: '300' },
@@ -20,7 +21,7 @@ def take_order(menus)
   end
   print '>'
   order_number = gets.to_i
-  puts "#{menus[order_number][:name]}(#{menus[order_number][:price]}円)ですね。"
+  puts "#{menus[order_number-1][:name]}(#{menus[order_number-1][:price]}円)ですね。"
   order_number
 end
 
@@ -30,5 +31,6 @@ order1 = take_order(DRINKS)
 puts 'フードメニューはいかがですか?'
 order2 = take_order(FOODS)
 
-total = FOODS[order1][:price].to_i + DRINKS[order2][:price].to_i
+total = FOODS[order2-1][:price].to_i + DRINKS[order1-1][:price].to_i
+#total = FOODS[order1-1][:price].to_i + DRINKS[order2-1][:price].to_i
 puts "お会計は#{total}円になります。ありがとうございました！"
